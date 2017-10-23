3bc9f727ff2e4bfdc4150d1d69bf1c99e09be2e8
goreleaserbot <goreleaser@carlosbecker.com>
2017-10-23 11:21:26 +0200

    gotsrpc version 0.10.0

--- gotsrpc.rb
+++ gotsrpc.rb
@@ -1,9 +1,9 @@
 class Gotsrpc < Formula
   desc "CLI utility to generate go and typescript RPC calls easily"
   homepage "https://github.com/foomo/gotsrpc"
-  url "https://github.com/foomo/gotsrpc/releases/download/0.9.0/gotsrpc_0.9.0_darwin_amd64.tar.gz"
-  version "0.9.0"
-  sha256 "0c4b7b58deaaa3988e9bdba4c7b474bd9e56d0a21bbe45189f9aa44a480b20f6"
+  url "https://github.com/foomo/gotsrpc/releases/download/0.10.0/gotsrpc_0.10.0_darwin_amd64.tar.gz"
+  version "0.10.0"
+  sha256 "dd0cd1323da2d2abade54bd96723be0556f4049497c74d31f9ac10bba88e6a7d"
 
   def install
     bin.install "gotsrpc"
