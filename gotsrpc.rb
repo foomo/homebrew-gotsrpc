a9efc5f5d4208c2f8bb571bb3c6f754306f91b8c
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-30 15:35:49 +0100

    Brew formula update for gotsrpc version 0.12.3

--- gotsrpc.rb
+++ gotsrpc.rb
@@ -1,9 +1,9 @@
 class Gotsrpc < Formula
   desc "CLI utility to generate go and typescript RPC calls easily"
   homepage "https://github.com/foomo/gotsrpc"
-  url "https://github.com/foomo/gotsrpc/releases/download/0.12.2/gotsrpc_0.12.2_darwin_amd64.tar.gz"
-  version "0.12.2"
-  sha256 "7d334a1002674b3d94a945d1b3cda2e6d6cdea09d845d84af2fd6296220d26d8"
+  url "https://github.com/foomo/gotsrpc/releases/download/0.12.3/gotsrpc_0.12.3_darwin_amd64.tar.gz"
+  version "0.12.3"
+  sha256 "0aaaec39209cf92132314b7184f2311ff5b22ef9ff58574bda6c9fc18c278786"
 
   def install
     bin.install "gotsrpc"
