349d15a8b92599f0cfe7062ab3972860d19c18b0
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-31 13:52:41 +0200

    Brew formula update for gotsrpc version v2.0.9

--- gotsrpc.rb
+++ gotsrpc.rb
@@ -5,20 +5,20 @@
 class Gotsrpc < Formula
   desc "CLI utility to generate go and typescript RPC calls easily"
   homepage "https://github.com/foomo/gotsrpc"
-  version "2.0.8"
+  version "2.0.9"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/foomo/gotsrpc/releases/download/v2.0.8/gotsrpc_2.0.8_darwin_amd64.tar.gz"
-      sha256 "e01addcd2bbc7fb5708c79917689fe24668132efe0a85cbd40b21202b1352615"
+      url "https://github.com/foomo/gotsrpc/releases/download/v2.0.9/gotsrpc_2.0.9_darwin_amd64.tar.gz"
+      sha256 "a8f00c7549adadf18bd4a8211876f0f966125cd4e9a6a57f0519e845e69e40dd"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/foomo/gotsrpc/releases/download/v2.0.8/gotsrpc_2.0.8_linux_amd64.tar.gz"
-      sha256 "f0755af9d917a08c4d80e77e85762dc9312b7d590a867924a3a5c36b7463c796"
+      url "https://github.com/foomo/gotsrpc/releases/download/v2.0.9/gotsrpc_2.0.9_linux_amd64.tar.gz"
+      sha256 "d7956c62da6fcb5d21a9c63d5e5d15d60337319b96613f2f8ede24a303e3b080"
     end
   end
 
