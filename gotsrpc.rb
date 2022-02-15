9be09161a647083caa1d2a39c2fbf7e259853bb5
goreleaserbot <goreleaser@carlosbecker.com>
2022-02-15 16:22:49 +0100

    Brew formula update for gotsrpc version v2.2.0

--- gotsrpc.rb
+++ gotsrpc.rb
@@ -5,20 +5,20 @@
 class Gotsrpc < Formula
   desc "CLI utility to generate go and typescript RPC calls easily"
   homepage "https://github.com/foomo/gotsrpc"
-  version "2.1.2"
+  version "2.2.0"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/foomo/gotsrpc/releases/download/v2.1.2/gotsrpc_2.1.2_darwin_arm64.tar.gz"
-      sha256 "213a048026f1c303aff6b38c3e8804531148dbf54550b71e8a32f6bdd3e9f335"
+    if Hardware::CPU.intel?
+      url "https://github.com/foomo/gotsrpc/releases/download/v2.2.0/gotsrpc_2.2.0_darwin_amd64.tar.gz"
+      sha256 "7fcbf8141b4b5b21b86d308824bc768e32ff4160a146656e86323b1ec569f10a"
 
       def install
         bin.install "gotsrpc"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/foomo/gotsrpc/releases/download/v2.1.2/gotsrpc_2.1.2_darwin_amd64.tar.gz"
-      sha256 "e1a18d4907a8d4ec3c6f655f92e3f9592571c67f4db2528d2c65a8478ad57eb0"
+    if Hardware::CPU.arm?
+      url "https://github.com/foomo/gotsrpc/releases/download/v2.2.0/gotsrpc_2.2.0_darwin_arm64.tar.gz"
+      sha256 "455dce4f398e092f4d30155d364c8f1db6bc10b0f34a3d499462046f5035f369"
 
       def install
         bin.install "gotsrpc"
@@ -28,16 +28,16 @@ class Gotsrpc < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/foomo/gotsrpc/releases/download/v2.1.2/gotsrpc_2.1.2_linux_arm64.tar.gz"
-      sha256 "6257f04aaaee384801196202b822ab3e33b7abc2e9d4b85bed10602e464f3f9a"
+      url "https://github.com/foomo/gotsrpc/releases/download/v2.2.0/gotsrpc_2.2.0_linux_arm64.tar.gz"
+      sha256 "32a5e5d1bd3ec8eab0ddbfc655eac5d4155cf29fd1265097c81f1fa5c8edd458"
 
       def install
         bin.install "gotsrpc"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/foomo/gotsrpc/releases/download/v2.1.2/gotsrpc_2.1.2_linux_amd64.tar.gz"
-      sha256 "8c5f0a8640f1b0108e95d98e4b6ed4e39803cb896ff0655e5da9cf061d293ff2"
+      url "https://github.com/foomo/gotsrpc/releases/download/v2.2.0/gotsrpc_2.2.0_linux_amd64.tar.gz"
+      sha256 "d44d6fa175d249864a335cd9e62e508c297c3a0333e131ed353914fb8a774862"
 
       def install
         bin.install "gotsrpc"
